9acea51a8db6da29c9a8ccb56821f99516ff7c4e
Vedant Navale <129394506+AgeCoder@users.noreply.github.com>
2026-05-03 23:16:11 +0530

    testing cicd 05

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // API Configuration - uses environment variables in production, localhost in development
 
-const API_URL = import.meta.env.VITE_API_URL || "http://10.0.0.55:5000/";
+const API_URL = import.meta.env.VITE_API_URL || "https://project.alightage.in";
 
 export default API_URL;
